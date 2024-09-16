dfaaa21dd1251cfb5bfe4fbd151e07663dbd079e
tomoyuki <at01110833@gmail.com>
2024-09-17 03:12:35 +0900

    修正

--- cal.js
+++ cal.js
@@ -1,11 +1,8 @@
-import minimist from "minimist";
-
 const today = new Date();
 const thisYear = today.getFullYear();
 const thisMonth = today.getMonth();
-const option = minimist(process.argv.slice(2));
-const year = option["y"] ? option["y"] : thisYear;
-const month = option["m"] ? option["m"] : thisMonth + 1;
+const year = thisYear;
+const month = thisMonth + 1;
 
 const startDate = new Date(year, month - 1, 1);
 const endDate = new Date(year, month, 0);
